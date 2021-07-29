b780b9ce6fce333125658e94b19d70c7b066e353
Damien Arrachequesne <damien.arrachequesne@gmail.com>
2021-07-29 09:17:49 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+
+## Socket.IO minimal example
+
+```
+$ npm install           # install the dependencies
+$ npm start             # run the server
+```
+
+You can run this example online at:
+
+- [Replit](https://repl.it/github/socketio/socket.io-minimal-example)
+- [StackBlitz](https://stackblitz.com/edit/socketio-base?file=index.js)
---
+++ index.js
@@ -0,0 +1,34 @@
+const { readFileSync } = require("fs");
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+
+const httpServer = createServer((req, res) => {
+  if (req.url !== "/") {
+    res.writeHead(404);
+    res.end("Not found");
+    return;
+  }
+  // reload the file every time
+  const content = readFileSync("index.html");
+  const length = Buffer.byteLength(content);
+
+  res.writeHead(200, {
+    "Content-Type": "text/html",
+    "Content-Length": length,
+  });
+  res.end(content);
+});
+
+const io = new Server(httpServer, {
+  // Socket.IO options
+});
+
+io.on("connection", (socket) => {
+  console.log(`connect ${socket.id}`);
+
+  socket.on("disconnect", (reason) => {
+    console.log(`disconnect ${socket.id} due to ${reason}`);
+  });
+});
+
+httpServer.listen(3000);
